558a0e0aad99334f539c314d5416c5aa3d9395e7
MuhammadSodiq <muhammadsodiq571@protonmail.com>
2025-06-23 18:03:49 +0000

    update

--- .config/nvim/lua/config/autocmds.lua
+++ .config/nvim/lua/config/autocmds.lua
@@ -60,7 +60,7 @@ vim.api.nvim_create_autocmd('BufWinEnter', {
 })
 
 vim.api.nvim_create_autocmd("FileType", {
-  pattern = { "lua", "c", "jsonc" },
+  pattern = { "lua", "jsonc" },
   callback = function()
     vim.opt.tabstop = 2
     vim.opt.shiftwidth = 2
